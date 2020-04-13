9e1708e51d366aa7521c840a292531a78d6fc23f
Meghan Garg <mgarg@splunk.com>
2020-04-13 01:54:43 -0700

    Updating html content

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "My first flask application!!!"
+    user = {"username" : "meghan"}
+    posts = [{"quote" :"What a beautiful day"},{"quote": "We have a great weather today!"}]
+    return render_template('index.html',title='Home',user=user,posts=posts)
 
