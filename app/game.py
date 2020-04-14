ee84bc20ddd14ed8d3bb7a4c045ed4a12693619e
Meghan Garg <mgarg@splunk.com>
2020-04-13 20:58:27 -0700

    Adding secret number game code

--- app/game.py
+++ app/game.py
@@ -1,9 +1,43 @@
 from flask_wtf import FlaskForm
+import random
 from wtforms import IntegerField, SubmitField
 from wtforms.validators import DataRequired
 
+
+class GameLogic(object):
+    ALLOWED_ATTEMPTS = 7  # initialize with seven attempts
+    def __init__(self, attempts: int = 7, range: int = 100) -> object:
+        if attempts < self.ALLOWED_ATTEMPTS:
+            self.attempts = attempts
+            self.ALLOWED_ATTEMPTS = attempts
+        else:
+            self.attempts = self.ALLOWED_ATTEMPTS
+        self.secret_number = random.randrange(0, range)
+
+    def reset_game(self, range):
+        self.attempts = self.ALLOWED_ATTEMPTS
+        self.secret_number = random.randrange(0, range)
+
+    def verify_against_secret_number(self,number):
+        guessed_number = int(number)
+        self.attempts -=1
+        if not guessed_number is self.secret_number:
+            if guessed_number > self.secret_number:
+                message = f'Wrong! Try going lower. Total attempts left: {self.attempts}!!'
+            elif guessed_number < self.secret_number:
+                message = f'Wrong! Try going higher. Total attempts left: {self.attempts}!!'
+            if self.attempts == 0:
+                message = f"Hard Luck! You are out of attempts. The secret number was {self.secret_number} " \
+                          f" Starting a new Game..."
+                self.reset_game(100)
+            return message
+        else:
+            message = f"Great Job! You guessed the no. in  {self.ALLOWED_ATTEMPTS - self.attempts} attempts!! " \
+                      f"  Starting a new Game.."
+            self.reset_game(100)
+        return message
+
 class GamePanel(FlaskForm):
-    number = IntegerField(" Enter Number ", validators = [DataRequired()])
+    number = IntegerField(" Enter Number: ", validators=[DataRequired()])
     submit = SubmitField("Verify!")
-
-
+    logic = GameLogic()
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,21 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
-from app.game import GamePanel
+from app.game import GamePanel, GameLogic
 
 @app.route("/")
 @app.route("/index")
 def index():
-    user = {"username" : "meghan"}
+    user = {"username" : "User!"}
     posts = [{"quote" :"What a beautiful day"},{"quote": "We have a great weather today!"}]
     return render_template('index.html',title='Home',user=user,posts=posts)
 
 @app.route("/game", methods=['GET','POST'])
 def game():
-    panel = GamePanel()
-    user = {"username": "meghan"}
-    return render_template('game.html',title='Game', user=user,form=panel)
+    form = GamePanel()
+    user = {"username": "User!"}
+    if form.validate_on_submit():
+        flash(f'Number entered {form.number.data}')
+        message = form.logic.verify_against_secret_number(form.number.data)
+        flash(message)
+        # return render_template('game.html',title='Game', user=user,form=panel)
+    return render_template('game.html', user=user,form=form)
